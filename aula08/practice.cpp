dc24f4771d5bbe3b21ec987eb3e2c71fd1bfd2b3
Lohana-Caruccio <lohana472@gmail.com>
2026-05-16 17:40:39 -0300

    Prática nova

---
+++ aula08/practice.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>  
+#include <cstdlib> //Para usar o system("cls") e system("pause")
+
+using namespace std;
+
+int main(){
+
+    int n1, n2, res;
+    char opc;
+
+    inicio:
+
+    system("cls"); //Limpa a tela
+
+    cout << "Digite o valor da nota 1: ";
+    cin >> n1;
+    cout << "Digite o valor da nota 2: ";
+    cin >> n2;
+
+    res = (n1 + n2);
+
+    /*
+     >=60 - Aprovado
+     >= 40 < 60 - Recuperação
+     < 40 - Reprovado
+    */
+
+    if(res >= 60){
+        cout << "\nAprovado!";
+
+    }else if(res >= 40){
+        cout << "\nRecuperacao!";
+    }else{
+        cout << "\nReprovado!";
+    }
+
+    cout << "\nDeseja calcular outras notas? [s/n]: "; 
+    cin >> opc;
+
+    if(opc == 's' or opc == 'S'){
+        goto inicio;
+    } else {
+        cout << "\nPrograma finalizado!";
+    }
+
+    return 0;
+}
