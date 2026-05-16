065f56f6df193a05e8133b31d1c5b5852fb6d043
Lohana-Caruccio <lohana472@gmail.com>
2026-05-16 15:30:00 -0300

    Prática nova

---
+++ aula05/practice.cpp
@@ -0,0 +1,19 @@
+//Incremento e Decremento
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //n1 = n1 + 1 ou n1 += 1;
+    //n1++; // ++: Adiciona 1 ao valor de uma variável
+    //n1--; //--: Diminui 1 do valor de uma variável
+    //n1++; (PÓS INCREMENTO: O valor é usado antes de ser incrementado, imprime o que ele era) e ++n1; (PRÉ INCREMENTO: O valor é incrementado antes de ser usado, e já imprimi com incremento)
+
+    int n1 = 10;
+
+    cout << "n1: "<< n1 <<endl;
+    cout << "n1: "<< --n1 <<endl;
+    
+
+    return 0;
+}
