1ff0042eb9388a26bcdc42ecc9ef5162e737ad8a
Lohana-Caruccio <lohana472@gmail.com>
2026-05-16 14:33:08 -0300

    Prática nova

---
+++ aula03/practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415  //Definição de constante
+#define canal cout << "Canal CFB\n";
+
+int main(){
+
+    //int vidas, tiros, life; - Declarações de múltiplas variáveis do mesmo tipo
+    int vidas = 3, tiros = 500, life = 100;
+
+    cout << "\n" << "PI: " << pi << "\n\n";
+    canal;
+
+    return 0;
+}
