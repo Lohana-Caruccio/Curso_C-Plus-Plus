b5fc7698ec7f2e3d7223497f0d7e35812fe3d29e
Lohana-Caruccio <lohana472@gmail.com>
2026-05-16 15:57:01 -0300

    Prática nova

---
+++ aula06/practice.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num = 10;
+
+    cout << num << endl;
+
+    //num = num * -1;
+    //num *= -1;
+    num = -num;
+
+    //cout << -num << endl; Imprimi o valor invertido temporariamente
+    cout << num << endl;
+
+    return 0;
+}
