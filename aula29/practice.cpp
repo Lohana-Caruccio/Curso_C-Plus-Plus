e65e988086b37193574a323db78992c656c6ea98
Lohana-Caruccio <lohana472@gmail.com>
2026-06-16 17:42:48 -0300

    novas práticas

---
+++ aula29/practice.cpp
@@ -0,0 +1,23 @@
+// Ponteiros pt.2
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int *p;
+    int vetor[10];
+
+    p = vetor; // o nome do vetor é o endereço do primeiro elemento, mesma coisa que p = &vetor[0];
+    *p = 10; // vetor[0] = 10;
+    cout << "\n" << vetor[0] << endl; // imprime o endereço do segundo elemento do vetor
+
+    *(p+=1);
+    *p = 20;
+    cout << "\n" << vetor[1] << endl; 
+
+    *(p+=1);
+    *p = 30;
+    cout << "\n" << vetor[2] << endl;
+
+    return 0;
+}
