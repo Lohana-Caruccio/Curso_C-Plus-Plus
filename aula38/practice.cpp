6ddc9b39d9e4cef229f8ab1db511e1d72bc17734
Lohana-Caruccio <lohana472@gmail.com>
2026-07-17 17:29:42 -0300

    novas práticas

---
+++ aula38/practice.cpp
@@ -0,0 +1,21 @@
+// FOR_EACH
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+
+    vector<int>n{0,1,2,3,4,5,6,7,8,9};
+    
+
+    for_each(n.begin(),n.end(),[](int num) {
+        if (num % 2 == 0){
+            cout << num << endl;
+        }
+    });
+
+
+    return 0;
+}
